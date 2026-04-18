212840671fc7fdbef54803f86b5c95a604c836a7
Huynh Do Hoang Nguyen <nguyenhuynh120305@gmail.com>
2026-04-18 20:59:01 +0700

    resolve conflict

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,7 @@
 import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
+import 'views/login_screen.dart';
+import 'views/register_screen.dart';
 
 void main() {
   runApp(const MainApp());
@@ -10,7 +13,8 @@ class MainApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return const MaterialApp(
-      home: Scaffold(body: Center(child: Text('Hello World!'))),
+      home: LoginScreen(),
+      debugShowCheckedModeBanner: false,
     );
   }
 }
